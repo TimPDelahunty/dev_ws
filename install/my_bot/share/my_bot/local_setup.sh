48853ab99f18a967bd1de06ce92dbce924d8ad27
tim <tim.p.delahunty@gmail.com>
2024-12-16 14:33:54 +1000

    tilt and pan working

---
+++ install/my_bot/share/my_bot/local_setup.sh
@@ -0,0 +1,1 @@
+/home/tim/dev_ws/build/my_bot/ament_cmake_environment_hooks/local_setup.sh
--- src/my_bot/launch/rsp.launch.py
+++ src/my_bot/launch/rsp.launch.py
@@ -29,6 +29,14 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    node_joint_state_publisher = Node(
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher',
+    output='screen'
+)
+
+
 
     # Launch!
     return LaunchDescription([
@@ -37,5 +45,6 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
+        node_joint_state_publisher,
         node_robot_state_publisher
     ])
